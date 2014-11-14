06b5b0d9ae2f17cfb88497561c8311938742100c
yrmt <youri.mout@gmail.com>
2014-11-14 10:20:38 +0100

    Ajoute une fonction de cleanup pour fermer et supprimer le fifo.

--- server.c
+++ server.c
@@ -4,33 +4,45 @@
 #include <sys/stat.h> //Required by mkfifo - 
 #include <unistd.h> //Required by write - close - read
 #include <fcntl.h> //Required by open
+#include <signal.h>
 
 #define MSG 256
+#define FIFO "server.fifo"
+
+void cleanup(int inputFifo) {
+    struct stat info;
+    close(inputFifo);
+    if (unlink(FIFO) < 0) {
+        if (stat(FIFO, &info) == -1) {
+            printf("The fifo file doesn't exist\n");
+        } else {
+            printf("Couldn't delete fifo\n");
+        }
+        exit(EXIT_FAILURE);
+    }
+    exit(EXIT_SUCCESS);
+}
 
 int main(int argc, char const *argv[])
 {
-	int inputFifo;
-	char fifo_name[] = "server.fifo";
-	char message[MSG];
+    int inputFifo;
+    char message[MSG];
+    sigset(SIGINT, &cleanup);
 
-	if(mkfifo(fifo_name, 0777) != 0)
-	{
-		printf("Was unable to create input fifo\n");
-		exit(-1);
-	}
+    if(mkfifo(FIFO, 0777) != 0) {
+        printf("Was unable to create input fifo\n");
+        return -1;
+    }
 
-	while(1) //Horrible loop
-	{
-		if((inputFifo = open(fifo_name, O_RDONLY)) == -1)
-		{
-			printf("Was unable to open the server's fifo\n");
-		}
-		read(inputFifo, message, MSG);
-			printf("%s", message);
+    if ((inputFifo = open(FIFO, O_RDONLY)) == -1) {
+        printf("Was unable to open the server's fifo\n");
+        return -1;
+    }
 
-		close(inputFifo);
-		}
+    while(1) {
+        read(inputFifo, message, MSG);
+        printf("%s", message);
+    }
 
-	return 0;
-
+    return 0;
 }
