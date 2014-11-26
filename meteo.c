4fb8ace4dbbcc4acbbaf5264abd292ce91402446
yrmt <youri.mout@gmail.com>
2014-11-26 22:20:11 +0100

    fail

--- meteo.c
+++ meteo.c
@@ -9,7 +9,7 @@ int gen_atis(void){
     strncpy(ATIS[0], "EBLG 1803 00000KT 0600 FG OVC008 BKN040 PROB40 2024 0300 DZ FG OVC002 BKN040", MSG_SIZE);
     strncpy(ATIS[1], "EBBR 0615 20015KT 8000 RA SCT010 OVC015 TEMPO 0608 5000 RA BKN005 BECMG 0810 9999 NSW BKN025", MSG_SIZE);
 
-    if ((fichierLock = open(FICHIERLOCK, O_RDWR | O_CREAT, S_IRUSR | S_IRGRP | S_IROTH)) == FAIL {
+    if ((fichierLock = open(FICHIERLOCK, O_RDWR | O_CREAT, S_IRUSR | S_IRGRP | S_IROTH)) == FAIL) {
 		printf("Impossible de faire le fichier lock");
         return FAIL;
     }
