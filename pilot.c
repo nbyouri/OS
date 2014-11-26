a7c56971134c2904f815ad58a67a5d6b22aa0860
yrmt <youri.mout@gmail.com>
2014-11-26 18:49:30 +0100

    exit at the right time

--- pilot.c
+++ pilot.c
@@ -5,54 +5,52 @@ void pilot_cleanup(int, int, int);
 int main(void) {
     int server = -1;
     int out_server = -1;
-    
+
     struct request req;
     char response[MSG_SIZE];
-    
+
     req.pid = getpid();
     memcpy(req.msg, PILOT_REQUEST, MSG_SIZE);
     req.siz = strlen(req.msg);
-    
+
     if((server = open(FIFO_FILE, O_WRONLY)) == FAIL) {
-        
+
         printf("Couldn't open fifo %s\n", FIFO_FILE);
-        
+
     } else {
-        
+
         printf("Successfully opened %d\n", server);
         printf("Sending REQUEST...\n");
-        
+
         if (write(server, &req, sizeof(req)) == FAIL) {
-            
+
             printf("Failed to write message\n");
             pilot_cleanup(server, out_server, FAIL);
-            
+
         } else {
-            
+
             if ((out_server = open(FIFO_FILE_OUT, O_RDONLY)) == FAIL) {
-                
+
                 printf("Couldn't open output file...\n");
                 pilot_cleanup(server, out_server, FAIL);
-                
+
             } else {
-                
+
                 printf("Successfully opened %d\n", out_server);
-                
+
                 if (read(out_server, response, MSG_SIZE) == FAIL) {
-                    
+
                     printf("Failed to read response from output fifo\n");
                     pilot_cleanup(server, out_server, FAIL);
-                    
+
                 } else {
-                    
+
                     printf("Got response ! => %s\n", response);
-                    
+
                 }
             }
-            pilot_cleanup(server, out_server, EXIT_SUCCESS);
         }
-        
-        return EXIT_SUCCESS;
+        pilot_cleanup(server, out_server, EXIT_SUCCESS);
     }
 }
 
