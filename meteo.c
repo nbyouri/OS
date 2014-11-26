33366271f84bc51ebf572d023e65be366f4f777c
yrmt <youri.mout@gmail.com>
2014-11-26 22:13:39 +0100

    fix everything

--- meteo.c
+++ meteo.c
@@ -9,13 +9,13 @@ int gen_atis(void){
     strncpy(ATIS[0], "EBLG 1803 00000KT 0600 FG OVC008 BKN040 PROB40 2024 0300 DZ FG OVC002 BKN040", MSG_SIZE);
     strncpy(ATIS[1], "EBBR 0615 20015KT 8000 RA SCT010 OVC015 TEMPO 0608 5000 RA BKN005 BECMG 0810 9999 NSW BKN025", MSG_SIZE);
 
-    if((fichierLock = open(FICHIERLOCK, O_WRONLY | O_CREAT)) == FAIL){
+    if((fichierLock = open(FICHIERLOCK, O_RDWR | O_CREAT, S_IRUSR | S_IRGRP | S_IROTH)) == FAIL){
 		printf("Impossible de faire le fichier lock");
         return FAIL;
     }
 
     //ouverture, ectriture et fermeture du fichier meteo.txt
-    if((fichierTexte = open(FICHIERMETEO, O_WRONLY | O_CREAT)) == FAIL){
+    if((fichierTexte = open(FICHIERMETEO, O_RDWR | O_CREAT, S_IRUSR | S_IRGRP | S_IROTH)) == FAIL){
 		printf("Impossible d'ouvrir le fichier meteo\n");
         return FAIL;
     }
@@ -29,10 +29,6 @@ int gen_atis(void){
     close(fichierTexte);
     
     close(fichierLock);
-
-    /*if(unlink(FICHIERLOCK) == FAIL){
-			fatal("Impossible de detruire le fichier lock");
-    }*/
 
     return SUCCESS;
 }
