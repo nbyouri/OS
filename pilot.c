3f01eff652e40c9c42608e07aebc6002d69c6583
srozen <spat.monroe@gmail.com>
2014-11-26 16:33:16 +0100

    Added simple cleanup function to ensure close of the fifos at any level

--- pilot.c
+++ pilot.c
@@ -1,29 +1,80 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h> //Required by mkfifo - 
-#include <sys/stat.h> //Required by mkfifo - 
-#include <unistd.h> //Required by write - 
-#include <fcntl.h> //Required by open
+#include "global.h"
 
-#define MSG 256
+// TODO: add local cleanup
 
-int main(int argc, char const *argv[])
-{
-	int serverFifo;
-	char serverName[] = "server.fifo";
+void pilot_cleanup(int in_serv, int out_serv);
 
-	char message[MSG] = "You see me rollin \n"; //test message
-
-	while(1) //horrible loop
-	{
-
-		if((serverFifo = open(serverName, O_WRONLY)) == -1)
-		{
-			printf("Was unable to open the fifo\n");
-		}
-		write(serverFifo, message, MSG);
-		close(serverFifo);
-	}
-
-	return 0;
+int main(void) {
+    int server = -1;
+    int out_server = -1;
+    
+    struct request req;
+    char response[MSG_SIZE];
+    
+    req.pid = getpid();
+    memcpy(req.msg, PILOT_REQUEST, MSG_SIZE);
+    req.siz = strlen(req.msg);
+    
+    if((server = open(FIFO_FILE, O_WRONLY)) == FAIL) {
+        
+        printf("Couldn't open fifo %s\n", FIFO_FILE);
+        
+    } else {
+        
+        printf("Successfully opened %d\n", server);
+        printf("Sending REQUEST...\n");
+        
+        if (write(server, &req, sizeof(req)) == FAIL) {
+            
+            printf("Failed to write message\n");
+            pilot_cleanup(server, out_server);
+            
+        } else {
+            
+            if ((out_server = open(FIFO_FILE_OUT, O_RDONLY)) == FAIL) {
+                
+                printf("Couldn't open output file...\n");
+                pilot_cleanup(server, out_server);
+                
+            } else {
+                
+                printf("Successfully opened %d\n", out_server);
+                
+                if (read(out_server, response, MSG_SIZE) == FAIL) {
+                    
+                    printf("Failed to read response from output fifo\n");
+                    pilot_cleanup(server, out_server);
+                    
+                } else {
+                    
+                    printf("Got response ! => %s\n", response);
+                    
+                }
+            }
+            
+            pilot_cleanup(server, out_server);
+        }
+        
+        return EXIT_SUCCESS;
+    }
 }
+
+
+void pilot_cleanup(int in_serv, int out_serv){
+    // close the file descriptors
+    if (in_serv != -1) {
+        if (close(in_serv) == FAIL) {
+            printf("Couldn't close input file descriptor %d\n", in_serv);
+        } else {
+            printf("Successfully closed %d\n", in_serv);
+        }
+    }
+    if (out_serv != -1) {
+        if (close(out_serv) == FAIL) {
+            printf("Couldn't close output file descriptor %d\n", out_serv);
+        } else {
+            printf("Successfully closed %d\n", out_serv);
+        }
+    }
+}
+
